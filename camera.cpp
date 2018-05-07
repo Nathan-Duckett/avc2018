53dac72aa4209b37b6bf3bfd32c7c3d294fecf52
Nathan Duckett <nduckett13@gmail.com>
2018-05-08 11:35:44 +1200

    Camera split value test

--- camera.cpp
+++ camera.cpp
@@ -7,12 +7,21 @@
  
 int camera_read () {
 	take_picture();
-	int min = 256;
+	int min = 255;
 	int max = 0;
-	int pixels[320];
+	int pixels[8];
+	int temp_array[40];
+	int count = 0;
 	for (int i = 0; i < 320; i++){
-		int pixel = get_pixel(i, 120, 3);
-		pixels[i] = pixel;
+		int pixel = get_pixel(120, i, 3);
+		temp_array[count] = pixel;
+		if (count == 40) {
+			count = 0;
+
+			pixels[ ( i / 40 ) - 1 ] = temp_array;
+			temp_array = [];
+		}
+		count++;
 	}
 
 	for (int i = 0; i < 320; i++ ) {
@@ -39,7 +48,7 @@ int camera_read () {
 		pointValues[i + 160] = i;
 	}	
 
-	float sum = 0;
+	int sum = 0;
 
 	for (int i = 0; i < 320; i++) {
 		int weight = pointValues[i];
