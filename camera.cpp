b219f7b080490491c84726cd48bcbef4a7d1c4fb
Nathan Duckett <nduckett13@gmail.com>
2018-05-08 10:22:42 +1200

    Camera work

--- camera.cpp
+++ camera.cpp
@@ -6,6 +6,34 @@
  */
  
 int camera_read () {
+	take_picture();
+	int min = 256;
+	int max = 0;
+	int[] pixels = [];
+	for (int i = 0; i < 320; i++){
+		int pixel = get_pixel(i, 120, 3);
+		pixels[i] = pixel;
+	}
+
+	for (int i = 0; i < 320; i++ ) {
+		if (pixels[i] > max) {
+			max = pixels[i];
+		}
+		if (pixels[i] < min) {
+			min = pixels[i];
+		}
+	}
+	int average = (max - min) / 2;
+	int constantDiff = 30;
+
+	int blackValue = average + constantDiff;
+	int whiteValue = average - constantDiff;
+
+	if (average + constantDiff >= 235) {
+		blackValue = 235;
+	}
 	
+	
+
 	return 0;
 }
