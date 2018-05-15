16002242080bfd8d7217065039bb6db2ea168b59
Nathan Duckett <nduckett13@gmail.com>
2018-05-15 14:10:01 +1200

    Syntax error fixup

--- sensors.cpp
+++ sensors.cpp
@@ -8,7 +8,7 @@
 
 void motorspeedFor(int forSpeed){
 	set_motor(1,forSpeed);
-	set-motor(2,forSpeed);
+	set_motor(2,forSpeed);
 }
 
 void motorspeedLeft(int leftSpeed, int forSpeed){
@@ -23,22 +23,22 @@ void motorSpeedRight(int rightSpeed, int forSpeed){
 
 int sensor_reading () {
 	init();
-		int adc_readingFor;
-		int adc_readingLeft;
-		int adc_readingRight
-		
-		adc_readingFor = read_analog(0);
-		adc_readingLeft = read_analog(1);
-		adc_readingRight = read_analog(2);
-		if (adc_readingFor>0 && adc_readingFor<256){
-			speed = 127;
-			motorSpeed(speed);
-			sleep1(1,0);
+	int adc_readingFor;
+	int adc_readingLeft;
+	int adc_readingRight;
+
+	adc_readingFor = read_analog(0);
+	adc_readingLeft = read_analog(1);
+	adc_readingRight = read_analog(2);
+	if (adc_readingFor>0 && adc_readingFor<256){
+		speed = 127;
+		motorSpeed(speed);
+		sleep1(1,0);
 	}
-		if (adc_readingFor>255 && adc_readingFor<511){
-			speed = 0;
-			motorSpeed(speed);
-			sleep1(1,0);
-		}
+	if (adc_readingFor>255 && adc_readingFor<511){
+		speed = 0;
+		motorSpeed(speed);
+		sleep1(1,0);
+	}
 	return 0;
 }
