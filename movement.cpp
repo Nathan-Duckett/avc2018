aa40a8bb1a4d732b48a0ce8bba75fbd4ff58fa75
tanmichael1 <38870686+tanmichael1@users.noreply.github.com>
2018-05-22 10:13:00 +1200

    Add files via upload
    Added backward movement - aside from direction helper

--- movement.cpp
+++ movement.cpp
@@ -1,5 +1,6 @@
 #include "header.h"
 void go_straight(int delay, double PWM) ;
+void go_back(int delay, double PWM);
 void turn_left_sharp (int delay, double PWM);
 void turn_right_sharp (int delay, double PWM);
 void turn_left_slope (int delay, double PWM, double turning_distance);
@@ -31,16 +32,16 @@ int delay_to_microseconds (int delay) {
 /* Take the incoming data and choose which direction the robot should go */
 void direction_helper(int error_value) {
 	//Call one of the straight, left, or right methods.
-	if (error_value < -700) {
-		turn_left_sharp(1000, 0.5);
-	} else if (error_value < -200 && error_value > -700) {
-		turn_left_slope(1000, 0.5, 0.25);
-	} else if (error_value > -200 && error_value < 200) {
-		go_straight(10000, 1);
-	} else if (error_value > 200 && error_value < 700) {
-		turn_right_slope(1000, 0.5, 0.25);
-	} else if (error_value > 700) {
-		turn_right_sharp(1000, 0.5);
+	if (error_value < -1500) {
+		turn_left_sharp(5000, 0.5);
+	} else if (error_value < -400 && error_value >= -1500) {
+		turn_left_slope(5000, 0.4, 0.2);
+	} else if (error_value >= -400 && error_value <= 400) {
+		go_straight(5000, 0.75);
+	} else if (error_value > 400 && error_value <= 1500) {
+		turn_right_slope(5000, 0.4, 0.2);
+	} else if (error_value > 1500) {
+		turn_right_sharp(5000, 0.5);
 	}
 }
 
@@ -53,6 +54,24 @@ void go_straight(int delay, double PWM) {
 	double speed = PWM * 255;
 	set_motor(1, speed);
 	set_motor(2, speed);
+
+	sleep1 (seconds, microSeconds);
+
+	set_motor(1, 0);
+	set_motor(2, 0);
+
+	return;
+}
+
+void go_back(int delay, double PWM) {
+
+	int seconds = delay_to_seconds(delay);
+	int microSeconds = delay_to_microseconds(delay);
+
+	//Set the speed based off a constant factor (0 < PWM < 1)
+	double speed = PWM * 255;
+	set_motor(1, -speed);
+	set_motor(2, -speed);
 
 	sleep1 (seconds, microSeconds);
 
@@ -120,10 +139,11 @@ void turn_right_slope (int delay, double PWM, double turning_distance) {
   
 	int seconds = delay_to_seconds(delay);
 	int microSeconds = delay_to_microseconds(delay);
-
+	
+	double difference = turning_distance * 255;
 	double speed = PWM * 255;
 	set_motor(1, speed);
-	set_motor(2,  speed + turning_distance);
+	set_motor(2,  speed + difference);
 
 	sleep1 (seconds, microSeconds);
 	
